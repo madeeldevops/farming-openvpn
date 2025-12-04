0354b1e8d10a6e08a360d01c179959b735455a38
m.adeel3040@gmail.com <m.adeel3040@gmail.com>
2025-12-04 16:42:02 +0500

    sync logic interactive

--- install-cron.sh
+++ install-cron.sh
@@ -2,7 +2,7 @@
 # Update the crontab
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 CRON_LINE="* * * * * LXC_COUNT=${LXC_COUNT} ${SCRIPT_DIR}/ovpn-log-sync.sh >> ${HOME}/cron.log 2>&1"
-echo "[INFO] Updating the cronjob"
+echo "[INFO] Old cronjob file"
 crontab -l 2>/dev/null; echo "$CRON_LINE" | crontab -
 echo "[INFO] Updated crontab:"
 crontab -l
